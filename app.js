35732f006b1f867f978b58294686a9dcd9ffa5e4
f14c0 <jcvgalfonso@gmail.com>
2013-11-13 22:02:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express 		= require('express')
+var app 			= express()
+var record			= require('./models/record.js')
+var record_model	= new record()
+var routes 			= require('./routes/devices.js')
+var io      		= require('./lib/sockets.js').listen(app)
+var fs 				= require('fs');
+
+/**/
+/*app config*/
+app.configure(function () {
+	app.use(express.bodyParser())
+  	app.use(express.methodOverride())
+  	app.use(app.router)
+})
+
+//Specify where the static content is
+app.use(express.static('static', __dirname + '/static'))
+
+/*Init Model*/
+record_model.connect()
+
+/*Init Routes*/
+routes.init(app)
