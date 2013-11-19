a9be6d1ea7717e7182e39d8a8151a3e21560f337
f14c0 <jcvgalfonso@gmail.com>
2013-11-19 06:09:49 -0500

    various

--- models/record.js
+++ models/record.js
@@ -13,8 +13,11 @@ var record_schema = new schema({
 	rssi:     		{type: 	Number}	
 })
 //Colleciton name record
-record_model = mongoose.model('record',record_schema)
-module.exports.record_model= record_model
+//record_model = mongoose.model('record',record_schema)
+//module.exports.record_model= record_model
+
+//record_model = mongoose.model(config.location_id+'_record',record_schema)
+//module.exports.record_model= record_model
 
 var record = function(){
 
@@ -28,7 +31,25 @@ var record = function(){
 			}
 		})
 	}
-	this.insertRecord =function(mac,device_class,timestamp,sensor_id,rssi){
+
+	this.createModel= function(collection_name){
+		return mongoose.model(collection_name+'_record',record_schema)
+	}
+
+	this.getAllRecords = function(model, callback){
+		model.find(function(err, records){
+			callback(err, records)
+		})
+
+	}
+	this.findRecordsInRange= function(model, timeStamp_1, timeStamp_2, callback){
+		var query =model.find({timestamp: {$gte: timeStamp_1, $lte: timeStamp_2}}, callback);
+		query.exec(function(err, record){
+
+		})
+	}
+
+	this.insertRecord =function(model, mac,device_class,timestamp,sensor_id,rssi){
 		var record = new record_model()
 		record.mac=mac
 		record.device_class=device_class
@@ -43,11 +64,11 @@ var record = function(){
 			}
 		})
 	}
-	this.getAllRecords=function(callback){
+	/*this.getAllRecords=function(callback){
 		record_model.find(function(err,records){
 			callback(err,records)
 		})
-	}
+	}*/
 
 }
 module.exports=record
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,8 @@
 
 var record          = require('../models/record.js')
 var record_model    = new record()
+var matrix_helper   = require('../lib/matrix.js')
+var matrix          = new matrix_helper()
 var fs              = require('fs');
 var path            = require("path")
 
@@ -28,16 +30,38 @@ exports.init =function (app) {
   app.post('/API/matrix', function(req,res){
     console.log('matrix requested')
     res.writeHead(200,{'Content-Type':'application/json'})
-    var response= {"value_1":150,
-                    "value_2":20,
-                    "lat_1":-74.12569,
-                    "lng_1":40.98546,
-                    "lat_2":-74.12069,
-                    "lng_2":40.98046
+   
+    var response= {"value_1":48,
+                    "value_2":89,
+                    "lat_1":4.754508,
+                    "lng_1":-74.045985,
+                    "lat_2":4.631302,
+                    "lng_2":-74.067721
                   }
-    res.write(JSON.stringify(response))
+    var response2= {"value_1":35,
+                    "value_2":20,
+                    "lat_1":4.70948439,
+                    "lng_1":-74.110529,
+                    "lat_2":4.668482,
+                    "lng_2":-74.060270
+                  }
+    var response3= {"value_1":63,
+                    "value_2":20,
+                    "lat_1":4.571396,
+                    "lng_1":-74.139454,
+                    "lat_2":4.631302,
+                    "lng_2":-74.067721
+                  }
+
+    if(req.body.node_1=='PT_NORTE'&&req.body.node_2=='ES_MARLY'&& req.body.init_time==4){
+      res.write(JSON.stringify(response))
+    }else if(req.body.node_1=='PT_80'&&req.body.node_2=='ES_HEROES'&& req.body.init_time==4){
+      res.write(JSON.stringify(response2))
+    }else if(req.body.node_1=='PT_TUNAL'&&req.body.node_2=='ES_HEROES'&& req.body.init_time==4){
+
+    }
+    
     res.end()
-    console.log(req.body)
   })
   /*Config*/
   app.get('/config-file',function(req,res){
